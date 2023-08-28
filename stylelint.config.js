ed02e1f21b6eddcaf8ff1571315522ba1c3a18c4
Spiker985 <Spiker985@users.noreply.github.com>
2023-08-28 19:37:09 -0400

    chore(deps): update devdependency stylelint (#19) (#61)

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,14 +1,19 @@
 module.exports = {
-	extends: 'stylelint-config-standard',
+	extends: [
+		'stylelint-config-standard',
+	],
+	plugins: [
+		'stylelint-stylistic',
+	],
 	customSyntax: 'postcss-less',
 	rules: {
 		'declaration-empty-line-before': null,
 		'declaration-block-no-redundant-longhand-properties': null,
 		'no-invalid-position-at-import-rule': null,
 		'selector-pseudo-element-colon-notation': 'double',
-		'no-extra-semicolons': true,
+		'stylistic/no-extra-semicolons': true,
 		'function-no-unknown': null,
-		indentation: 'tab',
+		'stylistic/indentation': 'tab',
 		'selector-type-no-unknown': [
 			true, {
 				ignoreTypes: [
